1bd7797831cf6af9be66f161d948f285dde1598a
YamStranger <YamStranger@bk.ru>
2016-02-23 09:48:59 +0200

    feat: added project that will be used in testing how junit and jbehave works when tests are run in parallel

--- jbehave-in-parallel/src/test/java/net/serenitybdd/demos/actions/stepdefinitions/SimpleActionDefinitions.java
+++ jbehave-in-parallel/src/test/java/net/serenitybdd/demos/actions/stepdefinitions/SimpleActionDefinitions.java
@@ -6,6 +6,8 @@ import org.jbehave.core.annotations.Alias;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
+import net.thucydides.core.annotations.Managed;
+import org.openqa.selenium.WebDriver;
 
 public class SimpleActionDefinitions {
     @Given("I have prepared environment for $doSomething")
@@ -16,15 +18,21 @@ public class SimpleActionDefinitions {
     @Steps
     ATodoUser jane;
 
+    @Managed(driver = "chrome")
+    WebDriver chrome;
+
+    @Managed(driver = "phantomjs")
+    WebDriver phantomjs;
+
     @When("I perform \"$actionName\"")
     @Alias("I have performed \"$actionName\"")
     public void perform(String actionName) throws Throwable {
-        jane.perform(actionName);
+        jane.perform(actionName, chrome);
     }
 
     @Then("I expect result for \"$action\" should be \"$expected\"")
     @Alias("I have result for \"$action\" should be \"$expected\"")
     public void checkResultFor(String action, String expected) throws Throwable {
-        jane.checkResult(action, expected);
+        jane.checkResult(action, expected, phantomjs);
     }
 }
